1a04d7ecaa9f81e944ce599e8310081c4b87f130
CLIENTE <CLIENTE@Pontin>
2016-05-17 19:22:54 -0300

    Ajustes

--- src/br/univel/Gravacao.java
+++ src/br/univel/Gravacao.java
@@ -1,10 +1,9 @@
 package br.univel;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
@@ -15,7 +14,22 @@ import javax.xml.transform.stream.StreamResult;
 
 public class Gravacao {
 	
-	public static void main(String[]args) throws IOException{
+	public static void main(String[]args){
+		
+		try{
+			FileReader fr = new FileReader("arq.xml");
+			BufferedReader br = new BufferedReader(fr);
+			
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			
+			while((line = br.readLine())!= null){
+				sb.append(line).append("\n");
+		}
+			
+			catch(){
+			
+		}
 		
 		Cliente c = new Cliente();
 		c.setId(1);
